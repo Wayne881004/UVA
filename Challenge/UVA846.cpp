d90b5baca0fcbb1832192b9b5ec8449889a349d3
Wayne881004 <43371931+Wayne881004@users.noreply.github.com>
2019-05-30 21:05:38 +0800

    Add files via upload

---
+++ Challenge/UVA846.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+	freopen("input.in","r",stdin);
+	freopen("output.out","w",stdout);
+
+	int Kase;
+	scanf("%d",&Kase);
+	while(Kase--){
+		int a,b,dis,step = 0,sum = 0 ,len = 0;
+		scanf("%d %d",&a,&b);
+		dis = b-a;
+
+		while(1){
+			len++;
+			if(sum + 2*len > dis){
+				break;
+			}
+			sum += 2*len;
+			step += 2;
+		}
+		if(dis > (sum + len))
+			step += 2;
+		else if(sum != dis)
+			step += 1;
+
+		printf("%d\n",step);
+
+	}
+	return 0;
+}
